f7f1a2e17c3d670bba76d6b4c1729f3f288254bb
Alexis Vera <76126512+averadba@users.noreply.github.com>
2023-08-18 05:01:03 -0400

    Update screening_app.py

--- screening_app.py
+++ screening_app.py
@@ -19,7 +19,7 @@ initial_data = {
 }
 index = ["Test Positive", "Test Negative"]
 df = pd.DataFrame(initial_data, index=index)
-df_edited = st.data_editor(df) #Creating editable table
+df_edited = st.data_editor(df) #Creating editable tabl
 
 with st.beta_container():
     st.table(df)
